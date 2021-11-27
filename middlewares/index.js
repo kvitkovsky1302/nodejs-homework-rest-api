57b7f7a68f45ab701aa4cc4ce5ad6884dbf1ef91
Kvitkovsky Maxim <kvitkovsky1302@gmail.com>
2021-11-27 18:17:10 +0200

    add upload image

--- middlewares/index.js
+++ middlewares/index.js
@@ -1,9 +1,11 @@
 const controllerWrapper = require("./controllerWrapper");
 const { validation } = require("./validationMiddleware");
 const authenticate = require("./authenticate");
+const upload = require("./upload");
 
 module.exports = {
   controllerWrapper,
   validation,
   authenticate,
+  upload,
 };
